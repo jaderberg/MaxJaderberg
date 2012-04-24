08830700aa0dc3f03f9747b47f6092ea16fb4f5e
Max Jaderberg <maxjaderberg@gmail.com>
2012-04-24 10:31:56 +0100

    started blog

---
+++ blog.py
@@ -0,0 +1,19 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+import os
+
+BLOG_POSTS_DIR = os.path.join(os.path.dirname(__file__), 'blogposts')
+STATIC_FILES_DIR = os.path.join(os.path.dirname(__file__), 'static')
+
+
+def 
+
+application = webapp.WSGIApplication([
+                                        ('/.*', MainPage)
+                                    ], debug=False)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
